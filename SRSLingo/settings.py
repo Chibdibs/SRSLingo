e1b9d697c4ba4faeca6e6d31a5b19563f95c67fe
Nino Ross <9670771+Chibdibs@users.noreply.github.com>
2024-03-06 05:23:21 +0900

    Began developing the dashboard for the user.

--- SRSLingo/settings.py
+++ SRSLingo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9&hbl4%o4+l*t5_wk!*2spt+g^n=0i@lburg6&y@v+$n)4k&+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
@@ -121,6 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# Turn on during production testing and after setting debug to False
+# STATIC_URL = 'https://Chibdibs.github.io/SRSLingo/base/static/'
 STATIC_URL = 'static/'
 
 # Default primary key field type
