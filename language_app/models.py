aeef88bb464e48d8d89bba3a5d706cb29aac411a
Nino Ross <9670771+Chibdibs@users.noreply.github.com>
2024-04-25 00:37:22 +0900

    modified flashcard views.py and models.py

--- language_app/models.py
+++ language_app/models.py
@@ -6,6 +6,6 @@ class Flashcard(models.Model):
     back_text = models.CharField(max_length=255)
     # Consider adding more fields like user ownership, timestamps, categories, etc.
 
-    def __str__(self): # test
+    def __str__(self):
         return self.front_text[:50]
-# testing 2
+# // uhgghjigjk
