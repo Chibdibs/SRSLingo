133f5aecdbfc0a05dc44ca5d2c6f6a5db9a97f7b
Nino Ross <9670771+Chibdibs@users.noreply.github.com>
2024-06-24 00:16:08 +0900

    modified flashcard views.py and models.py

--- language_app/models.py
+++ language_app/models.py
@@ -8,3 +8,10 @@ class Flashcard(models.Model):
 
     def __str__(self):
         return self.front_text[:50]
+
+# sdfsdfsdfsd
+# sdfsdfsdfsd
+# sdfsdfsdfsd
+# sdfsdfsdfsd
+# sdfsdfsdfsd
+# sdfsdfsdfsd
